d36a27520d5d5f3c9745d28480ff7e4a8501349d
yocajii <yocajii@stone.sakura.ne.jp>
2023-03-06 14:40:09 +0900

    Add single conversation

--- lib/chatgpt_cli.rb
+++ lib/chatgpt_cli.rb
@@ -1,28 +1,41 @@
 # frozen_string_literal: true
 
 require 'byebug'
+require 'cli/ui'
 require 'dotenv/load'
 require 'openai'
 
 class ChatgptCli
-  class Error < StandardError; end
-
-  def self.hello
+  def initialize
     OpenAI.configure do |config|
       config.access_token = ENV['OPENAI_ACCESS_TOKEN']
     end
-
-    client = OpenAI::Client.new
-
-    response = client.chat(
-      parameters: {
-        model: 'gpt-3.5-turbo',
-        messages: [
-          { role: 'user', content: 'こんにちは！' }
-        ]
-      }
-    )
-    pp response
-    puts response.dig('choices', 0, 'message', 'content')
+    @client = OpenAI::Client.new
   end
+
+  def self.start
+    new.start
+  end
+
+  def start
+    puts 'Hint: Type "quit" to end this conversation.'
+    loop do
+      question = CLI::UI.ask('Enter your message.', default: 'Hello!')
+      break if question.downcase == 'exit'
+
+      puts 'Waiting...'
+      response = @client.chat(
+        parameters: {
+          model: 'gpt-3.5-turbo',
+          messages: [
+            { role: 'user', content: question }
+          ]
+        }
+      )
+      pp response
+      puts response.dig('choices', 0, 'message', 'content')
+    end
+  end
+
+  class Error < StandardError; end
 end
