24b1ec81ab699d37ee453373e25f3d3fd9f24ae6
yocajii <yocajii@stone.sakura.ne.jp>
2023-03-07 10:52:10 +0900

    Add display header

--- lib/chatgpt_cli/talk.rb
+++ lib/chatgpt_cli/talk.rb
@@ -3,15 +3,14 @@
 require 'byebug'
 
 module ChatgptCli
-
   class Talk
     def initialize
       @messages = []
     end
+
     def start(client)
-      puts 'Hint: To send your message, press "Enter" and "Ctrl + d".'
-      puts 'Hint: To exit this conversation, press "Ctrl + d" without any message.'
-      puts ''
+      display_header
+
       loop do
         user_content = await_user_content
         break if user_content.empty?
@@ -22,7 +21,19 @@ module ChatgptCli
 
     private
 
+    def display_header
+      hints = <<~MSG
+        Hint 1: To send your message, press "Enter" and "Ctrl + d".
+        Hint 2: To exit this conversation, press "Ctrl + d" without any message.
+      MSG
+      puts
+      puts 'C h a t G P T  C L I'.center(80)
+      puts '*' * 80
+      puts hints
+      puts '*' * 80
+    end
     def await_user_content
+      puts
       print 'You: '
       content = $stdin.readlines.join
       @messages << { role: 'user', content: }
@@ -30,8 +41,9 @@ module ChatgptCli
     end
 
     def await_assistant_content(client)
+      puts
       print 'Vanilla: '
-      content = client.fetch(@messages)
+      content = client.fetch(@messages).sub!(/^\R/, '')
       @messages << { role: 'assistant', content: }
       puts content
     end
